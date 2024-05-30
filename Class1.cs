ead96545c05349a25f2dc365571cc3f230a26ec1
Lenovo <Lenovo@DESKTOP-7JC1JF6>
2024-05-30 12:11:35 +0300

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,45 @@
+﻿
+using CodeReview;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<User> lstUser=new List<User>();
+        lstUser.Add(new User("Khaled", "123"));
+
+        Console.WriteLine("Enter UserName: ");
+        string userNameInput = Console.ReadLine();
+
+        Console.WriteLine("Enter Password: ");
+        string userPasswordInput = Console.ReadLine();
+
+        bool userNameIsFound = false;
+        for (int i = 0; i < lstUser.Count(); i++)
+        {
+            if (lstUser[i].UserName== userNameInput)
+            {
+                userNameIsFound = true;
+                if (lstUser[i].Password== userPasswordInput)
+                {
+                    Console.WriteLine("logged in");
+                }
+                else
+                {
+                    Console.WriteLine("Wrong Passowrd");
+                    break;
+                }
+
+            }
+                
+
+        }
+
+
+
+
+
+
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.Design;
+
+namespace CodeReview;
+
+internal class User
+{
+    public string UserName { get; set; }
+    public string Password { get; private set; }
+
+    public User(string userName, string password)
+    {
+        UserName = userName;
+        Password = password;
+    }
+    
+}
